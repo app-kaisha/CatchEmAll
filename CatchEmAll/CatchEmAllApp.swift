05a4d6b9de1c2205bb9862e7c9ca94f0ca3c07ed
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-09 17:48:37 +0000

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,18 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by app-kaihatsusha on 09/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
